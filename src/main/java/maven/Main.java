a25d3669842546716154f07ae25b438c77dec762
lucas-alencar <lucasalencar.rd@gmail.com>
2022-10-03 23:59:00 -0300

    Finalizado, resta substituir o método sleep pelas requisições reais

--- src/main/java/maven/Main.java
+++ src/main/java/maven/Main.java
@@ -4,7 +4,22 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String args[]) throws Exception {
-		rollDices rolldice = new rollDices();
-		rolldice.roll();
+		int[] testes = {5, 10, 25, 50, 150, 300, 450};
+		rollDices rolldice;
+		long start, elapsed;
+		
+		for(int testeAtual : testes) {
+			for(int i=0;i<20;i++) {
+				start = System.currentTimeMillis();
+				for(int j=0;j<testeAtual;j++) {
+					Thread.sleep(100);
+					/*rolldice = new rollDices();
+					rolldice.roll();*/
+				}
+				elapsed = System.currentTimeMillis() - start;
+				System.out.println("Tempo passado no teste "+testeAtual+
+						" Versão "+ (i+1)+ ": "+elapsed+"ms");
+			}
+		}
 	}
 }
--- src/main/java/maven/rollDices.java
+++ src/main/java/maven/rollDices.java
@@ -2,34 +2,39 @@ package maven;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class rollDices {
 	
 	private Response response;
+	private final OkHttpClient client = new OkHttpClient();
 	
 	public rollDices(){
 		this.response = null;
 	}
 	
 	private boolean request() throws IOException {
-		OkHttpClient client = new OkHttpClient();
 
-		Request request = new Request.Builder()
-			.url("https://roll-dice1.p.rapidapi.com/rollDice")
-			.get()
-			.addHeader("X-RapidAPI-Key", "3517a6f0afmshdf368e62e182f28p1803f1jsnb7b1349ebe06")
-			.addHeader("X-RapidAPI-Host", "roll-dice1.p.rapidapi.com")
-			.build();
-		
-		try {
-			this.response = client.newCall(request).execute();
+	    Request request = new Request.Builder()
+				.url("https://roll-dice1.p.rapidapi.com/rollDice")
+				.get()
+				.addHeader("X-RapidAPI-Key", "3517a6f0afmshdf368e62e182f28p1803f1jsnb7b1349ebe06")
+				.addHeader("X-RapidAPI-Host", "roll-dice1.p.rapidapi.com")
+				.build();
+	    
+	    try (Response response = client.newCall(request).execute()) {
+	      if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+			this.response = this.client.newCall(request).execute();
 			return true;
-		}catch(IOException exception){
-			exception.printStackTrace();
-		}
+	    }catch(IOException e) {
+	    	System.out.println(e.getStackTrace());
+	    }
 		
 		this.response = null;
 		return false;
